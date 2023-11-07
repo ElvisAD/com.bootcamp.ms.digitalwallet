3bcd0af8500d2abaf5ff2fdbb2e3d0ef9ff630f4
Jose Ayay <jmessi-3@hotmail.com>
2023-11-07 14:43:33 -0500

    Se agrego SonarQube y documentaion Swagger al microservicio

--- src/main/java/com/bootcamp/ms/digitalwallet/controller/DigitalWalletController.java
+++ src/main/java/com/bootcamp/ms/digitalwallet/controller/DigitalWalletController.java
@@ -1,11 +1,12 @@
 package com.bootcamp.ms.digitalwallet.controller;
 
 import com.bootcamp.ms.digitalwallet.model.dto.DigitalWalletDto;
-import com.bootcamp.ms.digitalwallet.model.entity.DigitalWalletEntity;
 import com.bootcamp.ms.digitalwallet.service.DigitalWalletService;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,19 +18,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/bootcamp/ms/digitalwallet")
 @Slf4j
+@Api(value = "Billetera Digital Controller", description = "Operaciones CRUD para billetera digital")
 public class DigitalWalletController {
 
     @Autowired
     DigitalWalletService service;
 
     @PostMapping
-    public Maybe<ResponseEntity<DigitalWalletDto>> crearCard(@RequestBody DigitalWalletDto digitalWalletDto) {
+    @ApiOperation(value = "Crea una billetera digital", response = DigitalWalletDto.class, responseContainer = "List")
+    public Maybe<ResponseEntity<DigitalWalletDto>> crearWallet(@RequestBody DigitalWalletDto digitalWalletDto) {
         return Maybe.just(digitalWalletDto)
                 .flatMap(p -> service.create(p).toMaybe())
                 .map(ResponseEntity::ok);
     }
 
     @GetMapping("/{numberPhoneWallet}")
+    @ApiOperation(value = "Obtiene una billetera digital por su numero", response = DigitalWalletDto.class, responseContainer = "List")
     public Maybe<ResponseEntity<DigitalWalletDto>> obtenerNumberDigitalWallet(@PathVariable String numberPhoneWallet) {
 
         return service.getDigitalWalletPhone(numberPhoneWallet)
@@ -42,6 +46,7 @@ public class DigitalWalletController {
                         Maybe.just(ResponseEntity.internalServerError().build()));
     }
     @PatchMapping("/{phoneDigitalWallet}")
+    @ApiOperation(value = "Actualiza el saldo de una billetera digital por numero telefono", response = DigitalWalletDto.class, responseContainer = "List")
     public Completable actualizarDigitalWallet(
             @PathVariable String phoneDigitalWallet,
             @RequestParam double valor) {
